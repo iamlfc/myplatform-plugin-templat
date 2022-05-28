455f45c63f4dfad9c939e47eeee175262ba82b42
GitHub Action <action@github.com>
2022-05-28 09:21:25 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/iamlfc/myplatformplugintemplat/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.iamlfc.myplatformplugintemplat.services
+
+import com.github.iamlfc.myplatformplugintemplat.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
